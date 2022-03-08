0e8cc398f5b275793cb78829f46d447e2942a68a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-03-09 01:08:37 +0600

    form design done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import FormHandling from './components/FormHandling/FormHandling';
 
 function App() {
   return (
     <div className="App">
-      
+      <FormHandling></FormHandling>
     </div>
   );
 }
---
+++ src/components/FormHandling/FormHandling.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+
+const FormHandling = () => {
+    const handleInput=(e)=>{
+       console.log(e.target.value)
+    }
+    return (
+        <>
+           <Form>
+               <label>First Name:</label>
+               <input
+               name='fname'
+               type='text'
+
+               onChange={(e)=>handleInput(e)}
+               >
+                   
+               </input>
+               <br/>
+               <label>Last Name:</label>
+               <input
+               name='lname'
+               type='text'
+
+               onChange={(e)=>handleInput(e)}
+               />
+                      
+                      <br/>
+               <label>Email:</label>
+               <input
+               name='email'
+               type='email'
+
+               onChange={(e)=>handleInput(e)}
+               />
+                      <br/>
+            
+               <label>Password:</label>
+               <input
+               name='password'
+               type='password'
+
+               onChange={(e)=>handleInput(e)}
+               >
+               </input>
+               <br/>
+               <Button >Submit</Button>
+  
+           </Form> 
+        </>
+    );
+};
+
+export default FormHandling;
